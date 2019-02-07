37c050409502432dde497b876def9808a3a743e2
Vladimir Bychkovsky <vladimir.bychkovsky@gmail.com>
2019-02-06 22:58:33 -0500

    using random forest

--- notebook.py
+++ notebook.py
@@ -7,28 +7,28 @@ import ml
 if __name__ == "__main__":
 
     class MyClass:
-        def startup(self):
-            from sklearn.datasets import load_iris
-            from sklearn.linear_model import LogisticRegression
-            X, y = load_iris(return_X_y=True)
-            import pickle
-            clf = LogisticRegression(random_state=0, solver='lbfgs',
-                    multi_class='multinomial').fit(X, y)
-            self.mycrazymodel = clf
 
+        def trainModel(self):
+            from sklearn.ensemble import RandomForestClassifier
+            from sklearn.datasets import make_classification
+
+            X, y = make_classification(n_samples=1000, n_features=4,
+                    n_informative=2, n_redundant=0, random_state=0, shuffle=False)
+            clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+            clf.fit(X, y)
+            return clf
+
+        def startup(self):
+            self.mycrazymodel = self.trainModel()
 
         @ml.periodic(3)
         def trainAModel(self):
             print("training a new model...")
             print("pickle and store the model somewhere")
+            clf = self.trainModel() # this could be loading new data...
 
-            from sklearn.datasets import load_iris
-            from sklearn.linear_model import LogisticRegression
-            X, y = load_iris(return_X_y=True)
             import pickle
-            clf = LogisticRegression(random_state=0, solver='lbfgs',
-                    multi_class='multinomial').fit(X, y)
-            with open('skl_lr.pickle', 'wb') as outfile:
+            with open('skl.pickle', 'wb') as outfile:
                 pickle.dump(clf, outfile)
 
         @ml.callable
@@ -37,17 +37,16 @@ if __name__ == "__main__":
             # load model if not yet loaded
             if not hasattr(self, 'mycrazymodel'):
                 import pickle
-                from sklearn.linear_model import LogisticRegression
                 print("loading and unpickle the model...")
-                self.mycrazymodel = "SKLearnModel"
-                with open('skl_lr.pickle', 'rb') as modelfile:
+                with open('skl.pickle', 'rb') as modelfile:
                     self.mycrazymodel = pickle.load(modelfile)
-            print("making a prediction using '{}' model".format(self.mycrazymodel))
-            return 42
-            
-        def someOtherMethod(self, blah):
-            print("do something else: ", blah)
 
+            # make a prediction
+            pred = self.mycrazymodel.predict([[0, 0, 0, 0]])
+            print("making a prediction using '{}' model: {}".format(
+                self.mycrazymodel, pred))
+            return pred
+            
 
     print("Writing out this class to file...")
     ml.storeClass(MyClass, 'storage.dill')
